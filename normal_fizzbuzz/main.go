3891c29dae728093759fcf8b885991470711cabd
Mixko <apisitmaneerat@gmail.com>
2024-07-09 23:15:30 +0700

    [Normal r3] refactor when number is not fizz or buzz

--- normal_fizzbuzz/main.go
+++ normal_fizzbuzz/main.go
@@ -7,14 +7,8 @@ func main() {
 }
 
 func fizzBuzz(n int) string {
-	if n == 2 {
-		return "2"
-	}
 	if n == 3 {
 		return "Fizz"
 	}
-	if n == 4 {
-		return "4"
-	}
-	return "1"
+	return fmt.Sprintf("%d", n)
 }
