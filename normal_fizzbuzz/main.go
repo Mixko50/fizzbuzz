614953e2ef4adb61cf07ce544a650690e5b18491
Mixko <apisitmaneerat@gmail.com>
2024-07-09 18:34:42 +0700

    [Normal r2] reset

--- normal_fizzbuzz/main.go
+++ normal_fizzbuzz/main.go
@@ -5,29 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
-
-func fizzBuzz(n int) string {
-	if n == 3 {
-		return "Fizz"
-	}
-
-	if n == 5 {
-		return "Buzz"
-	}
-	if n == 6 {
-		return "Fizz"
-	}
-	if n == 9 {
-		return "Fizz"
-	}
-	if n == 10 {
-		return "Buzz"
-	}
-	if n == 12 {
-		return "Fizz"
-	}
-	if n == 15 {
-		return "FizzBuzz"
-	}
-	return fmt.Sprintf("%d", n)
-}
