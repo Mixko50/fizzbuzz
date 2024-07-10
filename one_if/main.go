50abc593beaa4e981356469cc3d8c690092ec7d6
Mixko <apisitmaneerat@gmail.com>
2024-07-10 22:58:44 +0700

    [OneIf r2] reset

--- one_if/main.go
+++ one_if/main.go
@@ -5,13 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 }
-
-func fizzBuzz(num int) string {
-	output := ""
-	output += map[bool]string{num%3 == 0: "Fizz"}[true]
-	output += map[bool]string{num%5 == 0: "Buzz"}[true]
-	if output == "" {
-		return fmt.Sprint(num)
-	}
-	return output
-}
