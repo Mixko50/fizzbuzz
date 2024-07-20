4b9f08db807311993c8ef4ba9bd9e95fce52317e
Mixko <apisitmaneerat@gmail.com>
2024-07-20 20:51:29 +0700

    [NoIf r1] reset

--- no_if/main.go
+++ no_if/main.go
@@ -5,12 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
-
-func fizzBuzz(n int) string {
-	output := ""
-	output += map[bool]string{true: "Fizz"}[n%3 == 0]
-	output += map[bool]string{true: "Buzz"}[n%5 == 0]
-	output += map[bool]string{true: fmt.Sprint(n)}[output == ""]
-
-	return fmt.Sprint(output)
-}
