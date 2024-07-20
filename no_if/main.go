b9b301741ef1aa68ff47980c0a0f88bddb9ab4a7
Mixko <apisitmaneerat@gmail.com>
2024-07-20 21:09:12 +0700

    [NoIf r3] case 5

--- no_if/main.go
+++ no_if/main.go
@@ -9,6 +9,7 @@ func main() {
 func FizzBuzz(n int) string {
 	output := ""
 	output += map[bool]string{true: "Fizz"}[n%3 == 0]
+	output += map[bool]string{true: "Buzz"}[n%5 == 0]
 	output += map[bool]string{true: fmt.Sprint(n)}[output == ""]
 	return output
 }
